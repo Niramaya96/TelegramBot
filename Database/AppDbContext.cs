c02d063fb058baf48aec9923dcfceaa7b811f1c8
Nik <nikitozer25@gmail.com>
2024-02-14 22:35:42 +0700

    Db

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TelegramBot.Models;
 
 namespace TelegramBot.Database
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Record> Records { get; set; }
+
+        public AppDbContext()
+        {
+            
+        }
 
     }
 }
--- Models/Record.cs
+++ Models/Record.cs
@@ -2,5 +2,8 @@
 {
     public class Record
     {
+        public int? OrderNumber { get; set; }
+        public int? Count { get; set; }
+        public string Description { get; set; }
     }
 }
